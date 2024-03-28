762d32d57a88a8f2bb2fc3e158977871a61ab9f0
Mateusz Jozwik <mateusz.jozwik@skydreams.com>
2024-03-28 16:09:06 +0100

    PLAT-1279 - resolve symfony 7.0 deprecations

--- DependencyInjection/ActivityLogExtension.php
+++ DependencyInjection/ActivityLogExtension.php
@@ -16,7 +16,7 @@ class ActivityLogExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
